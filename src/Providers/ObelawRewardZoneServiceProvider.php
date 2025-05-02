09f9f90e3afb233cc4e802185c831e89d65e85de
Karim Mohamed <komicho1996@gmail.com>
2025-05-02 15:55:32 +0300

    First commit

---
+++ src/Providers/ObelawRewardZoneServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Obelaw\RewardZone\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ObelawRewardZoneServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
